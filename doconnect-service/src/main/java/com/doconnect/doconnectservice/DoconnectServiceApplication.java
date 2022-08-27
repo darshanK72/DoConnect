99b8a8313dd152ec4110e2844177d107c8ec6926
RahulChilampande <chilampanderahul@gmail.com>
2022-08-27 12:49:18 +0530

    Created Eureka Server

--- doconnect-service/src/main/java/com/doconnect/doconnectservice/DoconnectServiceApplication.java
+++ doconnect-service/src/main/java/com/doconnect/doconnectservice/DoconnectServiceApplication.java
@@ -3,9 +3,11 @@ package com.doconnect.doconnectservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAsync
+@EnableEurekaClient
 public class DoconnectServiceApplication {
 
 	public static void main(String[] args) {
--- doconnect_ui/src/app/Service/authservice.service.ts
+++ doconnect_ui/src/app/Service/authservice.service.ts
@@ -2,31 +2,38 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthserviceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  login(username:string,password:string)
-  {
+  login(username: string, password: string) {
     let loginRequest = {
-      username:username,
-      password:password
-    }
-    const headers = new HttpHeaders({Authorization:'Basic ' + btoa(username+ ":" +password)});
-    return this.http.post("http://localhost:8080/login",loginRequest,{headers,responseType :'json'});
+      username: username,
+      password: password,
+    };
+    const headers = new HttpHeaders({
+      Authorization: 'Basic ' + btoa(username + ':' + password),
+    });
+    return this.http.post('http://localhost:8081/login', loginRequest, {
+      headers,
+      responseType: 'json',
+    });
   }
 
-  register(regObj:any)
-  {
-    return this.http.post("http://localhost:8080/user/register",regObj,{responseType:'text'});
+  register(regObj: any) {
+    return this.http.post('http://localhost:8081/user/register', regObj, {
+      responseType: 'text',
+    });
   }
 
-  getAllUsers(username:any,password:any)
-  {
-    const headers = new HttpHeaders({Authorization:'Basic ' + btoa(username+ ":" +password)}); 
-    return this.http.get("http://localhost:8080/user/get",{headers,responseType :'json'});
+  getAllUsers(username: any, password: any) {
+    const headers = new HttpHeaders({
+      Authorization: 'Basic ' + btoa(username + ':' + password),
+    });
+    return this.http.get('http://localhost:8081/user/get', {
+      headers,
+      responseType: 'json',
+    });
   }
 }
-
